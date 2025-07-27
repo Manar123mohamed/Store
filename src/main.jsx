32782608759480b1fcb04c3df8abe1a37c1316a0
Manar123mohamed <m01203408329@gmail.com>
2025-07-28 02:36:03 +0300

    Demoo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import './app.css';
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import CartContextProvider from "./components/context/CartContext.jsx";
